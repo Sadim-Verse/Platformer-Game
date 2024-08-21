cd0a4d27a143523d0f2a28e1c53a60317a2ae782
Ibraheem-Afolabi <saduibraheem@gmail.com>
2024-08-22 00:48:08 +0100

    Made the Orc's attack and cooldown logic
    Added an Attack and cooldown logic to the orc enemy

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -6,6 +6,7 @@ var speed = Global.enemy_parameters['soldier']['speed']
 @export var gravity := 300
 var speed_modifier := 1
 var attacking = false
+var attack_cooldown = false
 @onready var player = get_tree().get_first_node_in_group('player')
 
 func _process(delta):
@@ -19,18 +20,24 @@ func _process(delta):
 
 func animate():
 	$Sprite2D.flip_h = x_direction < 0
-	if attacking:
+	if attacking == true:
 		var difference = (player.position - position).normalized()
 		$Sprite2D.flip_h = difference.x < 0
-		if position.distance_to(player.position) < 50:
-			$AnimationPlayer.current_animation = 'Attack' 
-			speed_modifier = 0
+		if position.distance_to(player.position) < 50: 
+			attack_and_cooldown()
 		return	
 	$AnimationPlayer.current_animation = 'Walk' if x_direction else 'Idle'
-	
+
+func attack_and_cooldown():
+	if attack_cooldown == false:
+		$AnimationPlayer.current_animation = 'Attack'
+		print("Attack")
+		$Cooldown_timer.start(3)
+		attack_cooldown = true
+		speed_modifier = 0
 
 func check_player_distance():
-	if position.distance_to(player.position) < 200:
+	if position.distance_to(player.position) < 250:
 		attacking = true
 		speed_modifier = 0
 	else:
@@ -54,3 +61,18 @@ func  check_cliff():
 		x_direction = -1
 	if x_direction < 0 && !($FloorRays/Left.get_collider()):
 		x_direction = 1
+
+func cooldown_timer_timeout():
+	print("Idle")
+	$Attack_restarter.start(3)
+	$AnimationPlayer.current_animation = 'Idle'
+	
+func attack_restarter_timeout():
+	print("chek chek")
+	attack_cooldown = false
+
+func _on_cooldown_timer_timeout():
+	cooldown_timer_timeout()
+
+func _on_attack_restarter_timeout():
+	attack_restarter_timeout()
